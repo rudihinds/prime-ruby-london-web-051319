693385f460c0d40e9ebbb9e2ea26aa81681dd75f
rudi hinds <rudihinds@gmail.com>
2019-05-01 09:42:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,15 @@
-require 'prime'
-
-Prime.prime?(8753)
-
-
-Prime.prime?(4)
+def prime?(num)
+  # check if num is less than 2.
+  false if num < 2
+  
+  # check if num is divisible by any number
+  # between 2 to (num - 1)
+  # return false on the first instance
+  (2...num).each do |factor|
+    if (num % factor).zero? 
+      return false
+    end
+  end
+  
+  true # after checking all numbers and not receiving false
+end
